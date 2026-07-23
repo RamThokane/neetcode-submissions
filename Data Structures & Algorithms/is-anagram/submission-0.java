50127704aeefc927151755b87ed9c0f62ac67751
Ram Thokane <thokaneram@gmail.com>
2026-07-23 18:34:26 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[]r=s.toCharArray();
+        char[]a=t.toCharArray();
+
+        Arrays.sort(r);
+        Arrays.sort(a);
+
+        return Arrays.equals(r,a);
+
+
+
+
+    }
+    
+}
